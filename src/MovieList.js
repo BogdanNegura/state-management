caac8a3acb57eaf2ed3b3aa106c57130bd4a5fac
Bogdan Negura <id.bogdann@yahoo.com>
2019-08-04 00:27:20 +0100

    display data using from state using props ( name and price )

---
+++ src/MovieList.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Movie from './Movie'
+
+
+const MovieList = () => {
+
+    const [movies, setMovies] = useState([
+        {
+            name: 'Harry Potter',
+            price: '$10',
+            id: 23124
+        },
+        {
+            name: 'Game of Thrones',
+            price: '$10',
+            id: 2566124
+        },
+        {
+            name: 'Inception',
+            price: '$10',
+            id: 23524
+        }
+    ])
+
+    return (
+        <div>
+            {movies.map(movie => (
+                <Movie name={movie.name} price={movie.price} />
+            ))}
+        </div>
+    )
+}
+
+export default MovieList

5df42d3823d0ffc7e86cb0059c45bb2b2051c666
Bogdan Negura <id.bogdann@yahoo.com>
2019-08-04 17:50:32 +0100

    update

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { MovieContext } from './MovieContext'
+
 
 
 const Nav = () => {
-
+    const [movies, setMovies] = useContext(MovieContext)
     return (
         <div className="nav">
             <h3>Bogdan Negura</h3>
-            <p>List of Movies: </p>
+            <p>List of Movies: {movies.length}</p>
 
         </div>
     )

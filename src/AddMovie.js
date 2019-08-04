46754a60b09ba7e6da9339aa692646d84c6a14c3
Bogdan Negura <id.bogdann@yahoo.com>
2019-08-04 18:05:05 +0100

    create a component to add movies using the form and useSate and display on app

---
+++ src/AddMovie.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+
+const AddMovie = () => {
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState('')
+
+    const updateName = (e) => {
+        setName(e.target.value)
+    }
+    const updatePrice = (e) => {
+        setName(e.target.value)
+    }
+
+    const addMovie = e => {
+        e.preventDefault()
+        
+    }
+
+    return (
+        <form>
+            <input 
+                type="text" 
+                name="name" 
+                value={name} 
+                onChange={updateName}
+            />
+            <input 
+                type="text" 
+                name="price" 
+                value={price} 
+                onChange={updatePrice}
+            />
+            <button>Submit</button>
+        </form>
+    )
+}
+export default AddMovie
